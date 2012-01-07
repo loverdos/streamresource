4d766a03548ff58fdc734e394d80957244a80319
Christos KK Loverdos <loverdos@gmail.com>
2012-01-07 15:51:22 +0200

    Update working versions

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -60,16 +60,17 @@ class Project(info: ProjectInfo) extends DefaultProject(info) {
         </developer>
       </developers>;
 
-  val lib_maybe           = "com.ckkloverdos" %% "maybe"          % "0.3.0"        % "compile"  withSources()
-  val lib_sysprop         = "com.ckkloverdos" %% "sysprop"        % "0.1.0"        % "compile"  withSources()
-  val lib_converter       = "com.ckkloverdos" %% "converter"      % "0.3.0"        % "compile"  withSources()
+  val lib_maybe           = "com.ckkloverdos" %% "maybe"          % "0.4.0-SNAPSHOT"        % "compile"  withSources()
+  val lib_sysprop         = "com.ckkloverdos" %% "sysprop"        % "0.2.0-SNAPSHOT"        % "compile"  withSources()
+  val lib_converter       = "com.ckkloverdos" %% "converter"      % "0.4.0-SNAPSHOT"        % "compile"  withSources()
+  val lib_typedkey        = "com.ckkloverdos" %% "typedkey"       % "0.2.0-SNAPSHOT"        % "compile"  withSources()
   val lib_slf4j           = "org.slf4j"       % "slf4j-api"       % "1.6.1"        % "compile"  withSources()
   val lib_logback_simple  = "ch.qos.logback"  % "logback-classic" % "0.9.28"       % "test"     withSources()
   val lib_junit_interface = "com.novocode"    % "junit-interface" % "0.7"          % "test"
 
   override def testOptions =
     super.testOptions ++
-    Seq(TestArgument(TestFrameworks.JUnit, "-q", "-v"))
+    Seq(TestArgument(TestFrameworks.JUnit, "-q"))
 
   // Set up publish repository (the tuple avoids SBT's ReflectiveRepositories detection)
   private lazy val ScalaToolsReleases_t  = ("Scala Tools Releases"  -> "http://nexus.scala-tools.org/content/repositories/releases/")
